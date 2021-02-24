8a51ac3cc4ea05e84ac298cb8980bc545eeb2667
Cristian Martinez <cristianjmb10@gmail.com>
2021-02-24 16:13:45 -0500

    delete info

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -42,40 +42,6 @@ class ApiController extends Controller
         
     }
 
-    //Solo utilizar esta funcion para generar datos en la base de datos
-    // public function updateLanguage(){
-    //      Film::inRandomOrder()->limit(150)->update(array('language_id' => 6));
-    //      Film::inRandomOrder()->limit(150)->update(array('language_id' => 5));
-    //      Film::inRandomOrder()->limit(150)->update(array('language_id' => 4));
-    //      Film::inRandomOrder()->limit(200)->update(array('language_id' => 3));
-    //      Film::inRandomOrder()->limit(150)->update(array('language_id' => 2));
-    //      Film::inRandomOrder()->limit(100)->update(array('language_id' => 1));
-
-         
-    //      return 'ok';
-
-    // }
-
-    // public function updateLanguage(){
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 2021));
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 2015));
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 2010));
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 2005));
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 2000));
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 1990));
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 1980));
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 1970));
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 1960));
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 1950));
-    //      Film::inRandomOrder()->limit(100)->update(array('release_year' => 1940));
-
-
-         
-    //      return 'ok';
-
-    // }
-
-    
 
 
     
